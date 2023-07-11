74a01b082cfe15f65c2185d7ac4ea2df2999a101
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-11 13:08:18 +0530

    create user schema using mongoose

---
+++ Backend/models/userSchema.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+
+// Define the user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minLength: [5, "Name must be at least 5 characters"],
+      maxLength: [50, "Name must be less than 50 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: [true, "Email is already registered"],
+      lowercase: true,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiryDate: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods = {
+      jwtToken(){
+          return JWT.sign(
+            {id: this._id, email: this.email},
+            process.env.SECRET,
+            {expiresIn: '24h'}
+          )
+      }
+};
+
+// Create the User model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
