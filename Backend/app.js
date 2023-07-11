2590be5c9920bde247baf4644b9996c62b9472e7
shoaibhasann <shoaibhasan0940@gmail.com>
2023-07-11 17:13:05 +0530

    User Authentication Completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,32 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const connectDatabase = require('./config/db.js')
-const authRouter = require('./routes/authRoute.js');
-const cookieParser = require('cookie-parser');
+const connectDatabase = require("./config/db.js");
+const authRouter = require("./routes/authRoute.js");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// Connect to the database
 connectDatabase();
-// middleware setup
+
+// Middleware setup
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true,
+  })
+);
 
-app.use('/api/auth',authRouter);
+// Routes
+app.use("/api/auth", authRouter);
 
-app.use('/', (req,res) => {
-    res.status(200).json({
-        data: 'JWTauth Server updated'
-    })
+// Default route
+app.use("/", (req, res) => {
+  res.status(200).json({
+    data: "JWTauth Server updated",
+  });
 });
 
 module.exports = app;
